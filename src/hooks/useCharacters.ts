88bd09dc510aa55d033aebf3232f01f3e1dfab51
vmmiso <vmmiso@hotmail.com>
2024-03-10 13:44:42 +0100

    adding default fetch caracters limit

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { api, getCredentials } from '@apis/marvel';
 import { CharactersResponse } from '@interfaces';
 
-const fetchCharacters = async (name?: string, limit?: number) => {
+const fetchCharacters = async (name?: string, limit = 50) => {
   // const { ts, apiKey, hash } = getCredentials();
   const { data } = await api.get(`/public/characters`, {
     params: { ...getCredentials(), nameStartsWith: name, limit },
